5f7463af789fe2ddb44a63f01f21522625e175b7
Максим Василишин <maxim24152771@gmail.com>
2022-07-12 13:14:43 +0300

    Initial commit

---
+++ DB.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace shop
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=myshop");
+        
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace shop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String sql, Output = "";
+
+            DB db = new DB();
+            db.openConnection();
+
+            MySqlCommand command;
+            MySqlDataReader dataReader;
+
+            sql = "SELECT * FROM `products`";
+            command = new MySqlCommand(sql, db.getConnection());
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+            MessageBox.Show(Output);
+
+            dataReader.Close();
+            command.Dispose();
+            db.closeConnection();
+        }
+    }
+}
